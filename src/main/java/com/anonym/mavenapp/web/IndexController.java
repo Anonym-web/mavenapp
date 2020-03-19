e2ee4fa1378a7744954c74e24eddf4d79e40d1ce
Anonym <805705089@qq.com>
2020-03-19 15:20:25 +0800

    update the IndexController

---
+++ src/main/java/com/anonym/mavenapp/web/IndexController.java
@@ -0,0 +1,17 @@
+package com.anonym.mavenapp.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Author:Anonym
+ * 2020/3/19
+ */
+@Controller
+public class IndexController {
+
+    public void test(){
+
+
+
+    }
+}
