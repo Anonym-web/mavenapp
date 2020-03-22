d4c4c626c424fa055be815e3d445b36ce32139ce
Anonym <805705089@qq.com>
2020-03-22 15:41:55 +0800

    youIndex

---
+++ src/main/java/com/anonym/mavenapp/web/YouController.java
@@ -0,0 +1,16 @@
+package com.anonym.mavenapp.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Author:Anonym
+ * 2020/3/22
+ */
+@Controller
+public class YouController {
+
+    public void you(){
+
+
+    }
+}
